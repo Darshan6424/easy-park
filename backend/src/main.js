16baaf7c32794524c3210b745e094a47689ce007
kshitij khatri <kshitizkhatri81@gmail.com>
2025-12-29 11:15:12 +0545

    solve merge conflicts

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,11 +3,8 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
-<<<<<<< HEAD
 import bookingRoutes from "./routes/book.routes.js";
-=======
 import ParkingLocationRoutes from "./routes/parkingLocation.routes.js";
->>>>>>> main
 import { dbConnection } from "./lib/mongoDB.js";
 
 dotenv.config();
@@ -27,11 +24,8 @@ app.use(cookieParser());
 
 app.use(express.json());
 app.use("/api", authRoutes);
-<<<<<<< HEAD
 app.use("/api", bookingRoutes);
-=======
 app.use("/api/location", ParkingLocationRoutes);
->>>>>>> main
 
 app.listen(PORT, () => {
     console.log(`Server is listining to the port ${PORT}`);
