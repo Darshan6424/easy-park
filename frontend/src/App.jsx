e72d723a2f628c69cd518834d93b3d8466687aaf
kshitij khatri <kshitijkhatri437@gmail.com>
2025-12-30 20:11:42 +0545

    qr code scanner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,21 +4,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/layout.jsx";
 
 import Home from "./pages/home.jsx";
+import QRComponent from "./components/ui/QRComponent.jsx";
+import QrCodeScanner from "./components/ui/QRScanner.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <Home />
-            </Layout>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <div className="flex items-center justify-center w-full h-screen">
+      <QRComponent value={'hello guys'} />
+      <QrCodeScanner />
+    </div>
   );
 }
 
