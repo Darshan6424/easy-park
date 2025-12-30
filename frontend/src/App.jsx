f7441fb1747de173735565099d6cac97f210c66d
dayaj1222 <dayajoshi1222@gmail.com>
2025-12-30 18:13:19 +0545

    add theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div className='w-full h-screen bg-zinc-900 text-white flex items-center justify-center '>
+    <div className="w-full h-screen bg-zinc-900 text-white flex items-center justify-center ">
       hello world
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
