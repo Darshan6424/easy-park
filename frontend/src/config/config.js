619e99b8e1bd14fbc0a64de13395d35a67a0ded5
darshan6424 <kawasotiho@gmail.com>
2026-01-24 12:32:30 +0545

    fixed deploy problem

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -18,7 +18,7 @@ const APP_CONFIG = {
         secondary: "#2C3E50",
     },
     api: {
-        baseURL: "http://localhost:8000/api",
+        baseURL: import.meta.env.VITE_API_BASE_URL,
     },
 };
 
